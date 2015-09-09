036876f8fc76ff4781d07d7f83bbb8c088baabde
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2015-09-09 18:33:36 +0100

    A few minor changes to the batch processing function

--- GDAL_batch_processing.py
+++ GDAL_batch_processing.py
@@ -12,13 +12,39 @@ import os
 import shutil
 import subprocess
 
-def batch_convert(DataDirectory,raster_format):
+# This function looks for all the fiules of a certain format in a directory and
+# then translates them into a new format into a subdirectory named
+# after the target format. 
+def batch_convert(DataDirectory,raster_format,target_format):
  
     NewDataDirectory = LSDost.ReformatSeperators(DataDirectory)   
     DataDirectory = LSDost.AppendSepToDirectoryPath(NewDataDirectory)
-   
-     
-   
+
+    # Check the target format   
+    if target_format == "ENVI":
+        target_extension = ".bil"
+    elif target_format == "EHdr":
+        target_extension = ".bil"
+    elif target_format == "GTiff":
+        target_extension = ".tiff"
+    else:
+        print "You have not selcted a valid raster format!"
+        print "Options are ENVI, EHdr and GTiff"
+        target_extension = "NULL"   
+
+    # now make a directory   
+    if target_extension != "NULL":
+        
+        target_directory = DataDirectory+target_format
+
+        if not os.access(target_directory,os.F_OK):
+            print "Making path: "
+            os.mkdir(target_directory)
+            print "I made a directory: " + target_directory
+        else:
+            print "Path: " +target_directory+" already exists."     
+
+    # Now check the source format   
     if raster_format == "ENVI":
         raster_extension = ".bil"
     elif raster_format == "EHdr":
@@ -29,7 +55,8 @@ def batch_convert(DataDirectory,raster_format):
         print "You have not selcted a valid raster format!"
         print "Options are ENVI, EHdr and GTiff"
         raster_extension = "NULL"
-  
+
+    # find all the dataset of the source format  
     print "The data directory is: " + DataDirectory
     print "The raster extension is: " + raster_extension
     if raster_extension != "NULL":
