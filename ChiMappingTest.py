a5777a14f65385c87f2ca266467c6a1606ffdefd
Simon M. Mudd <simon.m.mudd@ed.ac.uk>
2016-12-18 20:23:49 +0000

    Added a routine for plotting channel numbers

--- ChiMappingTest.py
+++ ChiMappingTest.py
@@ -12,33 +12,37 @@ import numpy as np
 import LSDPlottingTools as LSDP
 
 def ChiMappingToolsTest():
-    DataDirectory = "/home/smudd/SMMDataStore/analysis_for_papers/Meghalaya/chi_analysis/"
+    #DataDirectory = "/home/smudd/SMMDataStore/analysis_for_papers/Meghalaya/chi_analysis/"
     #DataDirectory = "T:\\analysis_for_papers\\Meghalaya/chi_analysis\\"
-    Filename = "Mega_for_chi.bil"
-    HSFilename = "Mega_for_chi_hs.bil"
+    DataDirectory = "C:\\Vagrantboxes\\LSDTopoTools\\Topographic_projects\\Meghalaya\\"
+    Filename = "Mega_clip.bil"
+    HSFilename = "Mega_clip_hs.bil"
     
     DEMname = DataDirectory+Filename
     HSname = DataDirectory+HSFilename
     
     FigName = DataDirectory+'TestChiFull2.png'
-    ChiName = DataDirectory+'Mega_for_chi_MChiSegmented.csv'
+    ChiName = DataDirectory+'Mega_clip_MChiSegmented.csv'
     
     #FigFormat = 'svg'
     #FigFileN= 'Sorbas_chi.svg'
     #FigFileName= DataDirectory+FigFileN
     elevation_threshold = 30
     
-    LSDP.BasicChiPlotGridPlot(DEMname,HSname,ChiName, 'gray','gray',
-                            '$k_{sn}$',(0,0),
-                            0.4,FigName,'png',elevation_threshold)  
-    
+    #LSDP.BasicChiPlotGridPlot(DEMname,HSname,ChiName, 'gray','gray',
+    #                        '$k_{sn}$',(0,0),
+    #                        0.4,FigName,'png',elevation_threshold)  
+    FigName2 = DataDirectory+'TestChannelMap.png'    
+    LSDP.BasicChannelPlotGridPlotCategories(DEMname,HSname,ChiName, 'gray','gray',
+                            '$Channel$',(0,0),
+                            0.4,FigName2,'png',elevation_threshold,'source_key')      
     
     #LSDP.BasicDrapedPlotGridPlot(DEMname,HSname, 'gray','gray',
     #                        'Elevation in meters',(0,0),
     #                        0.4,FigName,'pdf')  
     
-    EPSG_string = LSDP.GetUTMEPSG(DEMname)
-    print "EPSG string is: " + EPSG_string
+    #EPSG_string = LSDP.GetUTMEPSG(DEMname)
+    #print "EPSG string is: " + EPSG_string
     
     
     #Plot the basin over the elevation    
--- LSDPlottingTools/LSDMap_ChiPlotting.py
+++ LSDPlottingTools/LSDMap_ChiPlotting.py
@@ -13,6 +13,7 @@ from osgeo import osr
 from os.path import exists
 from osgeo.gdalconst import GA_ReadOnly
 from numpy import uint8
+from cycler import cycler
 from matplotlib import rcParams
 import LSDMap_GDALIO as LSDMap_IO
 import LSDMap_BasicManipulation as LSDMap_BM
@@ -183,3 +184,167 @@ def BasicChiPlotGridPlot(FileName, DrapeName, chi_csv_fname, thiscmap='gray',dra
     else:
         plt.savefig(FigFileName,format=FigFormat,dpi=500)
         fig.clf()
+        
+def BasicChannelPlotGridPlotCategories(FileName, DrapeName, chi_csv_fname, thiscmap='gray',drape_cmap='gray',
+                            colorbarlabel='Elevation in meters',clim_val = (0,0),
+                            drape_alpha = 0.6,FigFileName = 'Image.pdf',FigFormat = 'show',
+                            elevation_threshold = 0, data_name = 'source_key'):
+    
+    import matplotlib.pyplot as plt
+    import matplotlib.lines as mpllines
+    from mpl_toolkits.axes_grid1 import AxesGrid
+    from matplotlib import colors
+
+    label_size = 10
+    #title_size = 30
+    axis_size = 12
+
+    # Set up fonts for plots
+    rcParams['font.family'] = 'sans-serif'
+    rcParams['font.sans-serif'] = ['arial']
+    rcParams['font.size'] = label_size 
+    #plt.rc('text', usetex=True)
+
+    # get the data
+    raster = LSDMap_IO.ReadRasterArrayBlocks(FileName)
+    raster_drape = LSDMap_IO.ReadRasterArrayBlocks(DrapeName)
+    
+    # now get the extent
+    extent_raster = LSDMap_IO.GetRasterExtent(FileName)
+    
+    x_min = extent_raster[0]
+    x_max = extent_raster[1]
+    y_min = extent_raster[2]
+    y_max = extent_raster[3]
+
+    # make a figure, sized for a ppt slide
+    fig = plt.figure(1, facecolor='white',figsize=(4.92126,3.5))
+
+    gs = plt.GridSpec(100,100,bottom=0.25,left=0.1,right=1.0,top=1.0)
+    ax = fig.add_subplot(gs[25:100,10:95])
+    
+    # This is the axis for the colorbar
+    ax2 = fig.add_subplot(gs[10:15,15:70])
+
+    #grid = AxesGrid(fig, 111, 
+    #                nrows_ncols=(1, 1),
+    #                axes_pad=(0.45, 0.15),
+    #                label_mode="1",
+    #                share_all=True,
+    #                cbar_location="right",
+    #                cbar_mode="each",
+    #                cbar_size="7%",
+    #                cbar_pad="2%",
+    #                )
+
+
+    # now get the tick marks    
+    n_target_tics = 5
+    xlocs,ylocs,new_x_labels,new_y_labels = LSDMap_BP.GetTicksForUTM(FileName,x_max,x_min,y_max,y_min,n_target_tics)  
+
+
+    print "xmax: " + str(x_max)
+    print "xmin: " + str(x_min)
+    print "ymax: " + str(y_max)
+    print "ymin: " + str(y_min)
+
+
+    #Z1 = np.array(([0, 1]*4 + [1, 0]*4)*4)
+    #Z1.shape = (8, 8)  # chessboard
+    #im2 = ax.imshow(Z1, cmap=plt.cm.gray, interpolation='nearest',
+    #             extent=extent_raster)  
+ 
+    #plt.hold(True)
+
+    im1 = ax.imshow(raster[::-1], thiscmap, extent = extent_raster, interpolation="nearest")
+    
+
+    # set the colour limits
+    print "Setting colour limits to "+str(clim_val[0])+" and "+str(clim_val[1])
+    if (clim_val == (0,0)):
+        print "Im setting colour limits based on minimum and maximum values"
+        im1.set_clim(0, np.max(raster))
+    else:
+        print "Now setting colour limits to "+str(clim_val[0])+" and "+str(clim_val[1])
+        im1.set_clim(clim_val[0],clim_val[1])
+   
+    plt.hold(True)
+   
+    # Now for the drape: it is in grayscale
+    #print "drape_cmap is: "+drape_cmap
+    im3 = ax.imshow(raster_drape[::-1], drape_cmap, extent = extent_raster, alpha = drape_alpha, interpolation="nearest")
+
+    # Set the colour limits of the drape
+    im3.set_clim(0,np.max(raster_drape))
+    
+    
+    ax.spines['top'].set_linewidth(1)
+    ax.spines['left'].set_linewidth(1)
+    ax.spines['right'].set_linewidth(1)
+    ax.spines['bottom'].set_linewidth(1) 
+     
+    #ax.spines['bottom'].set_capstyle('projecting')
+
+    #for spine in ax.spines.values():
+    #    spine.set_capstyle('projecting')
+
+
+    
+    ax.set_xticklabels(new_x_labels,rotation=60)
+    ax.set_yticklabels(new_y_labels)  
+    
+    ax.set_xlabel("Easting (m)")
+    ax.set_ylabel("Northing (m)")  
+
+    # This gets all the ticks, and pads them away from the axis so that the corners don't overlap        
+    ax.tick_params(axis='both', width=1, pad = 2)
+    for tick in ax.xaxis.get_major_ticks():
+        tick.set_pad(2)    
+
+    # Now we get the chi points
+    EPSG_string = LSDMap_IO.GetUTMEPSG(FileName)
+    print "EPSG string is: " + EPSG_string
+    
+    thisPointData = LSDMap_PD.LSDMap_PointData(chi_csv_fname) 
+    thisPointData.ThinData('elevation',elevation_threshold)
+    
+    # convert to easting and northing
+    [easting,northing] = thisPointData.GetUTMEastingNorthing(EPSG_string)
+    
+    
+    # The image is inverted so we have to invert the northing coordinate
+    Ncoord = np.asarray(northing)
+    Ncoord = np.subtract(extent_raster[3],Ncoord)
+    Ncoord = np.add(Ncoord,extent_raster[2])
+    
+    these_data = thisPointData.QueryData(data_name)
+    #print M_chi
+    these_data = [int(x) for x in these_data]
+    
+    
+    # make a color map of fixed colors
+    this_cmap = plt.cm.Paired
+    #bounds=[0,50,100,175,250,1205]
+    #norm = colors.BoundaryNorm(bounds, this_cmap.N)
+    
+    sc = ax.scatter(easting,Ncoord,s=0.5, c=these_data,cmap=this_cmap,edgecolors='none')
+
+    # This affects all axes because we set share_all = True.
+    ax.set_xlim(x_min,x_max)    
+    ax.set_ylim(y_max,y_min)     
+
+    ax.set_xticks(xlocs)
+    ax.set_yticks(ylocs)   
+    
+    cbar = plt.colorbar(sc,cmap=this_cmap,spacing='uniform', orientation='horizontal',cax=ax2)
+    cbar.set_label(colorbarlabel, fontsize=10)
+    ax2.set_xlabel(colorbarlabel, fontname='Arial',labelpad=-35)    
+
+    print "The figure format is: " + FigFormat
+    if FigFormat == 'show':    
+        plt.show()
+    elif FigFormat == 'return':
+        return fig 
+    else:
+        plt.savefig(FigFileName,format=FigFormat,dpi=500)
+        fig.clf()        
