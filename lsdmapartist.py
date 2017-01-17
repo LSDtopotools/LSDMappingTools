59ade8bdd23779cfb657f5c9f6ea5eebca061ea4
decvalts <declan.valters@gmail.com>
2017-01-17 13:48:03 +0000

    First commit of map artist

---
+++ lsdmapartist.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 16 21:50:53 2017
+
+LSDDrapeArtist
+
+@author: dav
+
+Object-based module for constructing
+drape maps in a reusable, generic way.
+
+"""
+
+class BaseRaster:
+    pass
+
+class DrapeRaster(BaseRaster):
+    pass
+
+class BackgroundRaster(BaseRaster):
+    pass
+
+class DrapeMap:
+    pass
+
+class MultiDrapeMap:
+    pass
