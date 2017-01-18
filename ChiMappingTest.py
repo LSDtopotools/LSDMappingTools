7e2068c7ff5c04b8289fbe4f50951eb0eae8a709
Simon M. Mudd <simon.m.mudd@ed.ac.uk>
2017-01-18 17:55:26 +0000

    Added a function to print basins over a fancy hillshade map

--- ChiMappingTest.py
+++ ChiMappingTest.py
@@ -14,13 +14,15 @@ import matplotlib.pyplot as plt
 
 def ChiMappingToolsTest():
     #DataDirectory = "/home/smudd/SMMDataStore/analysis_for_papers/Meghalaya/chi_analysis/"
-    #DataDirectory = "T:\\analysis_for_papers\\Meghalaya/chi_analysis\\"
-    DataDirectory = "C:\\Vagrantboxes\\LSDTopoTools\\Topographic_projects\\Meghalaya\\"
+    DataDirectory = "T:\\analysis_for_papers\\Meghalaya/chi_analysis\\"
+    #DataDirectory = "C:\\Vagrantboxes\\LSDTopoTools\\Topographic_projects\\Meghalaya\\"
     Filename = "Mega_clip.bil"
     HSFilename = "Mega_clip_hs.bil"
+    BasinFilename = "Mega_clip_AllBasins.bil"
     
     DEMname = DataDirectory+Filename
     HSname = DataDirectory+HSFilename
+    Basinname = DataDirectory+BasinFilename 
     
     FigName = DataDirectory+'TestChiFull2.png'
     ChiName = DataDirectory+'Mega_clip_MChiSegmented.csv'
@@ -70,14 +72,22 @@ def ChiMappingToolsTest():
     #======================================
 
     #======================================
+    # Uncomment this for a plot of the basins draped over a fancy hillshde map
+    FigName = DataDirectory+'BasinPlot.png'
+    LSDP.DrapedOverFancyHillshade(DEMname,HSname,Basinname, 'gray','cubehelix',
+                            '$k_{sn}$',(0,0),
+                            0.4,FigName,'png',elevation_threshold)  
+    #======================================    
+    
+    
+    #======================================
     # Uncomment this for a stack of gradient profiles 
-    FigName5 =  DataDirectory+'ChiGradientProfiles.png'
-    FigName6 =  DataDirectory+'FDGradientProfiles.png'
-    first_basin = 0
-    last_basin = 5
-    LSDP.StackedProfilesGradient(ChiName,FigName5,'png',elevation_threshold,first_basin,last_basin,plt.cm.afmhot,'chi',10,'log')  
-    LSDP.StackedProfilesGradient(ChiName,FigName6,'png',elevation_threshold,first_basin,last_basin,plt.cm.afmhot,'flow_distance',100000,'log')  
- 
+    #FigName5 =  DataDirectory+'ChiGradientProfiles.png'
+    #FigName6 =  DataDirectory+'FDGradientProfiles.png'
+    #first_basin = 0
+    #last_basin = 5
+    #LSDP.StackedProfilesGradient(ChiName,FigName5,'png',elevation_threshold,first_basin,last_basin,plt.cm.afmhot,'chi',10,'log')  
+    #LSDP.StackedProfilesGradient(ChiName,FigName6,'png',elevation_threshold,first_basin,last_basin,plt.cm.afmhot,'flow_distance',100000,'log')  
     #======================================
     
     #EPSG_string = LSDP.GetUTMEPSG(DEMname)
--- LSDPlottingTools/LSDMap_BasicPlotting.py
+++ LSDPlottingTools/LSDMap_BasicPlotting.py
@@ -822,6 +822,140 @@ def DrapedOverHillshade(FileName, DrapeName, thiscmap='gray',drape_cmap='gray',
 
 #==============================================================================
 
+##=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+## This function plots the chi slope on a shaded relief map
+##=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+def DrapedOverFancyHillshade(FileName, HSName, DrapeName, thiscmap='gray',drape_cmap='gray',
+                            colorbarlabel='Elevation in meters',clim_val = (0,0),
+                            drape_alpha = 0.6,FigFileName = 'Image.pdf',FigFormat = 'show',
+                            elevation_threshold = 0):
+    
+    import matplotlib.lines as mpllines
+    from mpl_toolkits.axes_grid1 import AxesGrid
+    from matplotlib import colors
+
+    label_size = 10
+    #title_size = 30
+    axis_size = 12
+
+    # Set up fonts for plots
+    rcParams['font.family'] = 'sans-serif'
+    rcParams['font.sans-serif'] = ['arial']
+    rcParams['font.size'] = label_size 
+    #plt.rc('text', usetex=True)
+
+    # get the data
+    raster = LSDMap_IO.ReadRasterArrayBlocks(FileName)
+    raster_HS = LSDMap_IO.ReadRasterArrayBlocks(HSName)
+    raster_drape = LSDMap_IO.ReadRasterArrayBlocks(DrapeName)
+
+    # now get the extent
+    extent_raster = LSDMap_IO.GetRasterExtent(FileName)
+    
+    x_min = extent_raster[0]
+    x_max = extent_raster[1]
+    y_min = extent_raster[2]
+    y_max = extent_raster[3]
+
+    # make a figure, sized for a ppt slide
+    fig = plt.figure(1, facecolor='white',figsize=(4.92126,3.5))
+
+    gs = plt.GridSpec(100,100,bottom=0.25,left=0.1,right=1.0,top=1.0)
+    ax = fig.add_subplot(gs[25:100,10:95])
+    
+    # This is the axis for the colorbar
+    ax2 = fig.add_subplot(gs[10:15,15:70])
+
+    #grid = AxesGrid(fig, 111, 
+    #                nrows_ncols=(1, 1),
+    #                axes_pad=(0.45, 0.15),
+    #                label_mode="1",
+    #                share_all=True,
+    #                cbar_location="right",
+    #                cbar_mode="each",
+    #                cbar_size="7%",
+    #                cbar_pad="2%",
+    #                )
+
+
+    # now get the tick marks    
+    n_target_tics = 5
+    xlocs,ylocs,new_x_labels,new_y_labels = GetTicksForUTM(FileName,x_max,x_min,y_max,y_min,n_target_tics)  
+
+
+    print "xmax: " + str(x_max)
+    print "xmin: " + str(x_min)
+    print "ymax: " + str(y_max)
+    print "ymin: " + str(y_min)
+
+    im1 = ax.imshow(raster[::-1], thiscmap, extent = extent_raster, interpolation="nearest")
+    
+
+    # set the colour limits
+    print "Setting colour limits to "+str(clim_val[0])+" and "+str(clim_val[1])
+    if (clim_val == (0,0)):
+        print "Im setting colour limits based on minimum and maximum values"
+        im1.set_clim(0, np.nanmax(raster))
+    else:
+        print "Now setting colour limits to "+str(clim_val[0])+" and "+str(clim_val[1])
+        im1.set_clim(clim_val[0],clim_val[1])
+   
+    plt.hold(True)
+   
+    # Now for the drape: it is in grayscale
+    #print "drape_cmap is: "+drape_cmap
+    HS_cmap = 'gray'
+    im2 = ax.imshow(raster_HS[::-1], HS_cmap, extent = extent_raster, alpha = 0.4, interpolation="nearest")
+
+    # Set the colour limits of the drape
+    im2.set_clim(0,np.nanmax(raster_HS))
+
+    # Now for the drape: it is in grayscale
+    #print "drape_cmap is: "+drape_cmap
+    im3 = ax.imshow(raster_drape[::-1], drape_cmap, extent = extent_raster, alpha = drape_alpha, interpolation="nearest")    
+
+    # Set the colour limits of the drape
+    im3.set_clim(0,np.nanmax(raster_drape))
+        
+    ax.spines['top'].set_linewidth(1)
+    ax.spines['left'].set_linewidth(1)
+    ax.spines['right'].set_linewidth(1)
+    ax.spines['bottom'].set_linewidth(1) 
+   
+    ax.set_xticklabels(new_x_labels,rotation=60)
+    ax.set_yticklabels(new_y_labels)  
+    
+    ax.set_xlabel("Easting (m)")
+    ax.set_ylabel("Northing (m)")  
+
+    # This gets all the ticks, and pads them away from the axis so that the corners don't overlap        
+    ax.tick_params(axis='both', width=1, pad = 2)
+    for tick in ax.xaxis.get_major_ticks():
+        tick.set_pad(2)    
+
+    # This affects all axes because we set share_all = True.
+    ax.set_xlim(x_min,x_max)    
+    ax.set_ylim(y_max,y_min)     
+
+    ax.set_xticks(xlocs)
+    ax.set_yticks(ylocs)   
+ 
+    cbar = plt.colorbar(im3,cmap=drape_cmap,spacing='uniform', orientation='horizontal',cax=ax2)
+    cbar.set_label(colorbarlabel, fontsize=10)
+    ax2.set_xlabel(colorbarlabel, fontname='Arial',labelpad=-35)        
+    
+    print "The figure format is: " + FigFormat
+    if FigFormat == 'show':    
+        plt.show()
+    elif FigFormat == 'return':
+        return fig 
+    else:
+        plt.savefig(FigFileName,format=FigFormat,dpi=500)
+        fig.clf()
+
+
+
+
 #==============================================================================
 # Make a simple hillshade plot
 def Hillshade(raster_file, azimuth = 315, angle_altitude = 45, NoDataValue = -9999): 
