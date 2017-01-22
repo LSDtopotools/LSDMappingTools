fea1f1206a3a24189dc05a02be4d5d94c6d76836
decvalts <declan.valters@gmail.com>
2017-01-22 14:53:26 +0000

    Changed __init__.py to Python 2/3 compatible with relative import.

--- LSDPlottingTools/__init__.py
+++ LSDPlottingTools/__init__.py
@@ -5,9 +5,11 @@ Created on Fri Oct 30 10:37:16 2015
 @author: smudd
 """
 
-from LSDMap_BasicPlotting import *
-from LSDMap_GDALIO import *
-from LSDMap_BasicManipulation import *
-from LSDMap_PointData import *
-from LSDMap_ChiPlotting import *
-from LSDMap_Subplots import *
+from __future__ import absolute_import
+
+from .LSDMap_BasicPlotting import *
+from .LSDMap_GDALIO import *
+from .LSDMap_BasicManipulation import *
+from .LSDMap_PointData import *
+from .LSDMap_ChiPlotting import *
+from .LSDMap_Subplots import *
