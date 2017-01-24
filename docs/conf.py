73662ded8b571e44db18c950ba7d531eef1f8e13
Declan Valters <decvalts@users.noreply.github.com>
2017-01-24 11:04:07 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['numpy', 'numpy.ma', 'gdal', 'osgeo', 'osgeo.gdal']
+MOCK_MODULES = ['numpy', 'numpy.ma', 'gdal', 'osgeo', 'osgeo.gdal', 'osgeo.gdalconst']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
