ce6f428f1fecfc06524ce9cde6c6e790bf4ec48a
decvalts <declan.valters@manchester.ac.uk>
2017-01-30 22:02:44 +0000

    Working drape plot in MapArtist

--- lsdmapartist.py
+++ lsdmapartist.py
@@ -17,6 +17,7 @@ This software is realsed under the Artistic Licence v2.0
 
 import LSDPlottingTools as LSDP
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class BaseRaster(object):
@@ -89,43 +90,94 @@ class DrapeRaster(BaseRaster):
     This class is for constructing the drape raster which gets overlaid on
     top of the BackgroundRaster
     """
-    def __init__(self, RasterName, Directory):
+    def __init__(self, RasterName, Directory,
+                 drape_min_threshold=None, drape_max_threshold=None):
+        
+        # Initialise the super-class
         super(DrapeRaster, self).__init__(RasterName, Directory)
         
-        self._render_drape(self.fullpath_to_raster)      
+        self._render_drape(self.fullpath_to_raster) 
+
+        self._drapeminthreshold = drape_min_threshold
+        self._drapemaxthreshold = drape_max_threshold
+        
+        if drape_min_threshold is not None:
+            self.mask_low_values()
+        if drape_max_threshold is not None:
+            self.mask_high_values()
+        
+    def mask_low_values(self):
+        low_values_index = self._RasterArray < self._drapeminthreshold
+        self._RasterArray[low_values_index] = np.nan
+                         
+    def mask_high_values(self):
+        high_values_index = self._RasterArray < self._drapemaxthreshold
+        self._RasterArray[high_values_index] = np.nan
         
     def _render_drape(self, fullpath_to_raster):
         pass
+    
+    def set_drape_min_threshold(self):
+        pass
+    
+    def set_drape_max_threshold(self):
+        pass
 
 class DrapePlot(object):
+    """
+    Class DrapeMap contains the methods for creating a drape map, i.e
+    a BackgroundRaster overlain with a DrapeRaster.
+    """
+    def __init__(self, DrapeRasterName, BackgroundRasterName, Directory,
+                 drape_colourmap,
+                 drape_min_threshold=None, drape_max_threshold=None,
+                 vmin=None, vmax = None):	
+        
+        self.Background = BackgroundRaster(BackgroundRasterName, Directory)
+        self.Drape = DrapeRaster(DrapeRasterName, Directory,
+                                 drape_min_threshold,
+                                 drape_max_threshold)
+        self._drape_colourmap = drape_colourmap
+        
+        self._vmin = vmin
+        self._vmax = vmax
+        
+        if drape_min_threshold is not None:
+            self.Drape.mask_low_values()
+        if drape_max_threshold is not None:
+            self.Drape.mask_high_values()
+        
+		
+    def make_drape_plot(self):
+	"""Creates a matplotlib Axes object with the drape map.
+		
+	Returns:
+	    A matplotlib.Axes instance containing the drape plot.
 	"""
-	Class DrapeMap contains the methods for creating a drape map, i.e
-	a BackgroundRaster overlain with a DrapeRaster.
-	"""
-    def __init__(self, DrapeRasterName, BackgroundRasterName, Directory):
-		
-		self._backgroundRaster = BackgroundRaster(BackgroundRasterName, Directory)
-		self._drapeRaster = DrapeRaster(DrapeRasterName, Directory)
-		
-	def make_drape_plot():
-		"""Creates a matplotlib Axes object with the drape map.
-		
-		Returns:
-		    A matplotlib.Axes instance containing the drape plot.
-		"""
-		return drape_plot_axes
+        fig, ax = plt.subplots()
+        self.im = ax.imshow(self.Background.Hillshade, "gray", interpolation="nearest")
+        self.im = ax.imshow(self.Drape._RasterArray, self._drape_colourmap, interpolation="nearest")
+        
+#    def mask_low_values(self):
+#    """Masks extreme high or small values."""
+#        self._drapeRaster.mask_low_values()
+        
+        
 
 class MultiDrapePlot(object):
-	"""
-	Class MultiDrapeMap creates multiplot drape maps.
-	
-	Composed of multiple DrapePlot
-	"""
+    """
+    Class MultiDrapeMap creates multiplot drape maps.
+    
+    Composed of multiple DrapePlot
+    """
     pass
 
 
-DataDirectory = "/mnt/SCRATCH/Dev/LSDMappingTools/test_rasters/peak_flow_rasters/"
-RasterName = "Elevations0.asc"
+#Directory = "/mnt/SCRATCH/Dev/LSDMappingTools/test_rasters/peak_flow_rasters/"
+Directory = "/run/media/dav/SHETLAND/Analyses/HydrogeomorphPaper/peak_flood_maps/boscastle/peak_flood/"
+BackgroundRasterName = "Elevations0.asc"
+DrapeRasterName = "WaterDepths2400_GRID_HYDRO.asc"
 
-raster = BaseRaster(RasterName, DataDirectory)
+#raster = BaseRaster(RasterName, DataDirectory)
+dp = DrapePlot(DrapeRasterName, BackgroundRasterName, Directory, "Blues", drape_min_threshold=0.05)
 
