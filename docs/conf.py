8c331f9ff3198279bf9690c72639a863c1c66e6b
Declan Valters <decvalts@users.noreply.github.com>
2017-02-01 16:15:03 +0000

    More mock modules

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ class Mock(MagicMock):
 MOCK_MODULES = ['scipy','pyproj', 'numpy', 'numpy.ma', 'gdal', 'osgeo', 'osgeo.gdal', 
                 'osgeo.gdalconst', 'osgeo.gdal_array', 'matplotlib', 'matplotlib.pyplot', 'matplotlib.rcParams',
                'matplotlib.colors', 'matplotlib.image', 'matplotlib.cm', 'matplotlib.gridspec', 'matplotlib.patches',
-               'cartopy', 'cartopy.feature', 'cartopy.crs']
+               'cartopy', 'cartopy.feature', 'cartopy.crs', 'cartopy.mpl.ticker', 'shapely', 'shapely.geometry.polygon']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
