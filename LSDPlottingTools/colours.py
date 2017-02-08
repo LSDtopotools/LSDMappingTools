88522b754ea35ce8ae4412ee88ed3e9c8637ff65
decvalts <declan.valters@gmail.com>
2017-02-08 21:56:16 +0000

    More colormap features

--- LSDPlottingTools/colours.py
+++ LSDPlottingTools/colours.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as _plt
 import matplotlib.gridspec as _gridspec
 import matplotlib.colors as _mcolors
 import matplotlib.cm as _cm
+from array import array
 
 from matplotlib.colors import LinearSegmentedColormap
 
@@ -198,6 +199,46 @@ class nonlinear_colourmap(LinearSegmentedColormap):
         levels.sort()
         print(levels)
         return levels
+
+
+class TransformedColourmap(object):
+    """ Applies function (which should operate on vectors of shape 3:
+    [r, g, b]), on colormap cmap. This routine will break any discontinuous
+    points in a colormap.
+    
+    DOES NOT WORK!
+    """
+    def __init__(self, function, cmap):
+        self.cdict = cmap._segmentdata
+        
+        step_dict = {}
+        # Firt get the list of points where the segments start or end
+        for key in ('red','green','blue'):
+            step_dict[key] = map(lambda x: x[0], self.cdict[key])
+            step_list = sum(step_dict.values(), [])
+            step_list = array(list(set(step_list)))
+        
+        # Then compute the LUT, and apply the function to the LUT
+        reduced_cmap = lambda step : array(cmap(step)[0:3])
+        old_LUT = array(map( reduced_cmap, step_list))
+        new_LUT = array(map( function, old_LUT))
+        
+        # Now try to make a minimal segment definition of the new LUT
+        cdict = {}
+        for i,key in enumerate(('red','green','blue')):
+            this_cdict = {}
+            for j,step in enumerate(step_list):
+                if step in self.step_dict[key]:
+                    this_cdict[step] = new_LUT[j,i]
+                elif new_LUT[j,i]!=old_LUT[j,i]:
+                    this_cdict[step] = new_LUT[j,i]
+                    
+            colorvector=  map(lambda x: x + (x[1], ), this_cdict.items())
+            colorvector.sort()
+            cdict[key] = colorvector
+
+    def __call__(self, function, cmap):
+        return _mcolors.LinearSegmentedColormap('colormap', self.cdict, 1024)
 
 
 class MidpointNormalize(_mcolors.Normalize):
