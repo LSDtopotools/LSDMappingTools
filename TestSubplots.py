136ebee10143ab10ad29bdcddf34f08edd473e13
fclubb <f.clubb@ed.ac.uk>
2017-02-15 13:45:20 +0000

    Small changes to subplots driver

--- TestSubplots.py
+++ TestSubplots.py
@@ -14,24 +14,21 @@ def make_field_sites_figure(DataDirectory):
     NRows = 3
     NCols = 2
     n_target_ticks = 6
-    
+
     LSDP.field_sites(DataDirectory, N_HSFiles, NRows, NCols, n_target_ticks)
-    
+
 def make_flood_maps(DataDirectory):
     LSDP.multiple_flood_maps(DataDirectory)
-    
+
 def flood_maps_with_shapefile(DataDirectory):
     LSDP.flood_maps_with_shapefile(DataDirectory)
 
 def flood_maps_shapefile_zoom(DataDirectory):
     LSDP.flood_maps_shapefile_with_zoom(DataDirectory)
-    
+
 if __name__ == "__main__":
-    DataDirectory = "/home/s0923330/Datastore/Python/floodplain_initiation/results_comparison/field_data/"
-    #make_field_sites_figure(DataDirectory)
-    make_flood_maps(DataDirectory)
+    DataDirectory = "/home/s0923330/Datastore/Python/floodplain_initiation/field_sites/"
+    make_field_sites_figure(DataDirectory)
+    #make_flood_maps(DataDirectory)
     #flood_maps_with_shapefile(DataDirectory)
     #flood_maps_shapefile_zoom(DataDirectory)
-    
-
-    
