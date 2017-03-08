b2e50ef7ef6c3384554df4fb746f137eeaa9a9f7
Simon M. Mudd <simon.m.mudd@ed.ac.uk>
2017-03-08 11:54:13 +0000

    Got the feature updaing to the GLIM codes

--- LSD_GeologyTools.py
+++ LSD_GeologyTools.py
@@ -230,28 +230,25 @@ def Rasterize_GLIM_geologic_maps_pythonic(shapefile_name):
     print("All done")        
 
 def GLIM_geologic_maps_modify_shapefile(shapefile_name):
-
+ 
     # The shapefile to be rasterized:     
     print('Rasterize ' + shapefile_name) 
     #get path and filename seperately 
     shapefilefilepath = LSDPT.GetPath(shapefile_name)
     shapefilename = LSDPT.GetFileNameNoPath(shapefile_name)
     shapefileshortname = LSDPT.GetFilePrefix(shapefile_name)
-
-    print("Shapefile name is: "+shapefilename)
-
-    # now get the the fields from the shapefile
-    daShapefile = shapefile_name
-
-    dataSource = ogr.Open(daShapefile,1)
-    daLayer = dataSource.GetLayer(0)
     
-    # lets see what the layers are
-    print("Let me tell you what the names of the fields are!")
-    layerDefinition = daLayer.GetLayerDefn()
-    for i in range(layerDefinition.GetFieldCount()):
-        print(layerDefinition.GetFieldDefn(i).GetName())        
-        
+    # get the new shapefile name
+    new_shapefile_name = shapefilefilepath+os.sep+shapefileshortname+"_new.shp"
+    
+    # copy the shapefile into the new shapefile--we don't wwant to mess up the original data
+    print("The New Shapefile name is: "+new_shapefile_name)
+    Copy_Shapefile(shapefile_name,new_shapefile_name)
+    
+    # New shapefile is opened for writing. 
+    dataSource = ogr.Open(new_shapefile_name,1)
+    daLayer = dataSource.GetLayer(0)
+      
     # add a new field
     new_field = ogr.FieldDefn("GEOL_CODE", ogr.OFTInteger)
     daLayer.CreateField(new_field)
@@ -262,26 +259,36 @@ def GLIM_geologic_maps_modify_shapefile(shapefile_name):
     for i in range(layerDefinition.GetFieldCount()):
         print(layerDefinition.GetFieldDefn(i).GetName())      
     
-      
     # Make a key for the bedrock
-    #geol_dict = dict()
-    #for feature in daLayer:
-    #    ID = feature.GetField("xx")
-    #    
-    #    if ID == "va":
-    #        feature.SetField("GEOL_CODE", 0)
-    #    elif ID == "vb":
-    #        feature.SetField("GEOL_CODE", 1)
-    #    elif ID == "ss":
-    #        feature.SetField("GEOL_CODE", 3)
-    #    else:
-    #        feature.SetField("GEOL_CODE", 4)
+    geol_dict = dict()
+    geol_iterator = 0
+    for feature in daLayer:
+        GEOL = feature.GetField("xx")
+        
+        if GEOL not in geol_dict:
+            geol_iterator = geol_iterator+1
+            print("I found a new rock type, GEOL: "+ str(GEOL)+ " and rock type: " + str(geol_iterator))
+            geol_dict[GEOL] = geol_iterator
+        
+        # now get the geol code
+        this_geol_code = geol_dict[GEOL]
+        # set the feature
+        feature.SetField("GEOL_CODE", this_geol_code)
+        
+        # need to update the layer        
+        daLayer.SetFeature(feature)
+            
+    print("The rocks are: ")
+    print(geol_dict)
       
     print("All done")   
 
 
 def Copy_Shapefile(shapefile_name,new_shapefile_name):
- 
+    """
+    Sweet Jesus why is this so difficult?
+    """
+    
     if exists(shapefile_name) is False:
         raise Exception('[Errno 2] No such file or directory: \'' + shapefile_name + '\'') 
         
@@ -294,8 +301,6 @@ def Copy_Shapefile(shapefile_name,new_shapefile_name):
 
     src = ogr.Open(shapefile_name)
     daLayer = src.GetLayer(0)
-
-
     
     # lets see what the layers are
     print("Let me tell you what the names of the fields are!")
@@ -358,10 +363,11 @@ if __name__ == "__main__":
     #shapefile_name = '/home/smudd/SMMDataStore/analysis_for_papers/Iberia_geology/SouthernSpain_geology.shp'
     #shapefile_name = 'T:\\analysis_for_papers\\Geology_raster\\bgs-50k_1726879\\sc034\\sc034_eyemouth_bedrock.shp' 
     
-    shapefile_name = 'C:\\VagrantBoxes\\LSDTopoTools\\Topographic_projects\\Iberia\\SouthernSpain_geology.shp'
-    new_shapefile_name = 'C:\\VagrantBoxes\\LSDTopoTools\\Topographic_projects\\Iberia\\New_SouthernSpain_geology.shp'    
+    shapefile_name = 'C:\\VagrantBoxes\\LSDTopoTools\\Topographic_projects\\Iberia\\TipOfSpain.shp'
+    new_shapefile_name = 'C:\\VagrantBoxes\\LSDTopoTools\\Topographic_projects\\Iberia\\New_TipOfSpain.shp'    
     
-    Copy_Shapefile(shapefile_name,new_shapefile_name)
+    
+    #Copy_Shapefile(shapefile_name,new_shapefile_name)
     #Rasterize_BGS_geologic_maps(shapefile_name)
     #Rasterize_GLIM_geologic_maps_pythonic(shapefile_name)
-    #GLIM_geologic_maps_modify_shapefile(shapefile_name)
+    GLIM_geologic_maps_modify_shapefile(shapefile_name)
