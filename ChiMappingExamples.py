2321c8014015b5a674baa1a7e9c73f413075cd59
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2017-06-27 17:04:01 +0100

    Added some comments to the example driver

--- ChiMappingExamples.py
+++ ChiMappingExamples.py
@@ -12,20 +12,38 @@ import LSDChiMappingExamples as CME
 
 if __name__ == "__main__":
 
-    
+    """
+    This is just a few lines for keeping track of how long the program is taking. 
+    You can ignore it.
+    """
     import time
     tic = time.clock()
 
-    # Change these filenames and paths to suit your own files
+    """
+    These lines tell the example functions where your files are. If you are using
+    the recommended file setup you won't need to modify these lines. 
+    
+    If you have set up your own directory structure you will need to modify
+    the directory names.
+    """
     DataDirectory = "T:\\analysis_for_papers\\Xian\\"
     #DataDirectory = 'C:\\VagrantBoxes\\LSDTopoTools\\Topographic_projects\\LSDTT_chi_examples\\'
     Base_file = 'Xian'
 
 
-    # This is for the first example. Uncomment to get a hillshade image
-    #CME.ExampleOne_PartOne_SimpleHillshade(DataDirectory,Base_file)
+    """
+    These lines are used to run the examples. Each line calls a function in the directory
+    ../LSDChiMappingExamples/
+    
+    The individual examples are different python scripts which you can inspect at your
+    lesiure. By playing with these scripts you can learn how to use our plotting tools. 
+    
+    To run the examples simply comment or uncomment these lines by adding or 
+    removing the comment symbol, #, below.
+    """
+    CME.ExampleOne_PartOne_SimpleHillshade(DataDirectory,Base_file)
     #CME.ExampleOne_PartTwo_PrintBasins(DataDirectory,Base_file)
-    CME.ExampleOne_PartThree_PrintBasinsWithLabels(DataDirectory,Base_file)
+    #CME.ExampleOne_PartThree_PrintBasinsWithLabels(DataDirectory,Base_file)
     
     toc = time.clock()
     print("This took: "+str(toc - tic)+" units of time")
