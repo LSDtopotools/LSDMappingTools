4f7fe39cb9f0767ab86b692a8b18229fe92912c4
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2017-07-21 17:44:14 +0100

    I have no idea what has changed but don't want a merge problem later

--- ChiMappingExamples.py
+++ ChiMappingExamples.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     """
     
     #MW.SimpleHillshade(DataDirectory,Base_file,cmap = "terrain", dpi = 250)
-    #MW.PrintBasins(DataDirectory,Base_file)
+    MW.PrintBasins(DataDirectory,Base_file)
     #MW.PrintBasins_Complex(DataDirectory,Base_file,Remove_Basins = [6], Rename_Basins = {3:"A",4:"B",8:"C",10:"D",12:"E"})
     #MW.PrintChannels(DataDirectory,Base_file)
     MW.PrintChannelsAndBasins(DataDirectory,Base_file, dpi = 300)
