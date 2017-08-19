c7d8c83cb162c2738396b832d790f75e9bc98fde
Fiona Clubb <f.clubb@ed.ac.uk>
2017-08-19 11:26:01 +0100

    Modifying model plotting to check for linux or windows

--- Create_hillshade_series_in_directory.py
+++ Create_hillshade_series_in_directory.py
@@ -8,10 +8,10 @@ This will produce a series of images that you can convert to a movie using ffmpe
 Here is a typical command line
 ffmpeg -framerate 5 -pattern_type glob -i '*.png' -vcodec libx264 -s 1230x566 -pix_fmt yuv420p movie7.mp4
 
-Some gotchas: 
+Some gotchas:
 The -framerate flag must come after ffmpeg (I think)
 You can be more specific about the pattern but this seems to work
-For the -s flag you must have pixel sizes that are divisible by 2. 
+For the -s flag you must have pixel sizes that are divisible by 2.
 
 Written by Simon Mudd
 June 2017
@@ -23,13 +23,14 @@ GPL3
 import LSDChiMappingExamples as CME
 import LSDMapWrappers as MW
 import os
+from sys import platform
 
 
 def get_filenames(root):
     # Create and empty list for the filenames
-    
+
     print("The root is: "+root)
-    
+
     these_filenames = []
     n = 0
     while 1:
@@ -42,24 +43,27 @@ def get_filenames(root):
             return these_filenames
 
 def run_plots(DataDirectory,Base_file):
-    
+
     root = DataDirectory+Base_file
     filenames = get_filenames(root)
     counter = 0
     for fname in filenames:
-        
+
         counter = counter+1
         # remove the extension form the files
         this_base_file = fname[:-4]
-        split_file = this_base_file.split("\\")
+        if "win" in platform:
+            split_file = this_base_file.split("\\")
+        elif "linux" in platform:
+            split_file = this_base_file.split("/")
         print("Split file is: ")
         print split_file
         this_base_file = split_file[-1]
         print("This base file is: "+ this_base_file)
-        
-        
+
+
         MW.SimpleHillshadeForAnimation(DataDirectory,this_base_file,cmap = "terrain", dpi = 250, imgnumber=counter, full_basefile = root)
-    
+
 
 
 if __name__ == "__main__":
@@ -83,7 +87,8 @@ if __name__ == "__main__":
     #DataDirectory = "T:\\analysis_for_papers\\Xian\\"
     #DataDirectory = 'C:\\VagrantBoxes\\LSDTopoTools\\Topographic_projects\\LSDTT_chi_examples\\'
     #Base_file = 'Xian2'
-    DataDirectory = 'S:\\movern_analysis\\model_runs\\muddpile\\big_fluvial_test\\'
+    #DataDirectory = 'S:\\movern_analysis\\model_runs\\muddpile\\big_fluvial_test\\'
+    DataDirectory = "/home/s0923330/muddpile_test/test1/"
     Base_file = 'LSDRM'
 
     """
@@ -96,8 +101,8 @@ if __name__ == "__main__":
     To run the examples simply comment or uncomment these lines by adding or
     removing the comment symbol, #, below.
     """
-    
+
     run_plots(DataDirectory,Base_file)
-    
-    toc = time.clock() 
+
+    toc = time.clock()
     print("This took: "+str(toc - tic)+" units of time")
