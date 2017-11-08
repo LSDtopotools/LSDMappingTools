7a6d8a195b4969064393f500e7dac859287dd983
bgailleton <boris.gailleton@gmail.com>
2017-11-08 17:48:04 +0000

    minor fixing on the plotting

--- LSDPlottingTools/LSDMap_KnickpointPlotting.py
+++ LSDPlottingTools/LSDMap_KnickpointPlotting.py
@@ -474,8 +474,8 @@ def chi_profile_knickzone(DataDirectory, fname_prefix, size_format='ESURF', FigF
         ax.fill_between(tKdf["chi"],0,tKdf[cumul_col], color = "k", alpha = 0.3)
 
         # Plotting the Chi profiles
-        ax2.plot(tCdf["chi"],tCdf["segmented_elevation"], lw = 1.2 , c ='#0089B9',alpha =0,zorder = 7)
-        ax2.scatter(tKdf["chi"],tKdf["elevation"], c = tKdf["sign"],cmap = 'RdBu_r', s = tKdf[knickpoint_col].abs(), alpha = 0, lw = 0.5, edgecolor = "k", zorder = 10)
+        ax2.plot(tCdf["chi"],tCdf["segmented_elevation"], lw = 1.2 , c ='#0089B9',alpha = 1,zorder = 7)
+        ax2.scatter(tKdf["chi"],tKdf["elevation"], c = tKdf["sign"],cmap = 'RdBu_r', s = tKdf[knickpoint_col].abs(), alpha = 0.75, lw = 0.5, edgecolor = "k", zorder = 10)
         
         # Plotting the derivative
         ax3.plot(tKdf["chi"],tKdf[deriv_cumul], lw = 0.7, c = '#E70B0B',alpha = 0.7,zorder = 5)
