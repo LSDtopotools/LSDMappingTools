d7f559b0a424335cbff858cd644e096375ef5c57
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2018-01-12 13:26:39 +0000

    Adding some changes to the point plotting functions

--- LSDMapWrappers/__init__.py
+++ LSDMapWrappers/__init__.py
@@ -8,4 +8,5 @@ Created on Fri Oct 30 10:37:16 2015
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from .LSDMapWrappers_BasicPlotting import *
+from .LSDMapWrappers_ChiPlotting import *
 
