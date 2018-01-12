cef962bc56bcd4cc750de4ca10309f953cc5c227
bgailleton <boris.gailleton@gmail.com>
2018-01-12 13:45:30 +0000

    Preparing merge

--- LSDMapFigure/PlottingRaster.py
+++ LSDMapFigure/PlottingRaster.py
@@ -1066,7 +1066,7 @@ class MapFigure(object):
                        max_point_size = 5, min_point_size = 0.5,
                        colour_log = False, colour_manual_scale = [],
                        manual_size = 0.5, alpha = 1, minimum_log_scale_cut_off = -10, label_field = "None",
-                       font_size = 6, offset = 100, zorder=1):
+                       font_size = 6, offset = 100, zorder=1, marker = "o", unicolor = "blue"):
         """
         This add point data to the map.
 
@@ -1171,7 +1171,7 @@ class MapFigure(object):
         print("I will plot the points now.")
         if len(this_data) == 0 or len(this_data) != len(easting):
             print("I am only plotting the points.")
-            sc = self.ax_list[0].scatter(easting,northing,s=point_scale, c="blue",cmap=this_colourmap,edgecolors='none', alpha = alpha,zorder=zorder)
+            sc = self.ax_list[0].scatter(easting,northing,s=point_scale, c= unicolor,cmap=this_colourmap,edgecolors='none', alpha = alpha,zorder=zorder, marker = marker)
         else:
             print("I will colour by the points")
             if(colour_manual_scale != []):
@@ -1183,13 +1183,13 @@ class MapFigure(object):
                     #scalarMap.set_array(tps_color)
                     #this_colourmap = scalarMap
                     #sc = self.ax_list[0].scatter(easting,northing,s=point_scale, c=tps_color,cmap=this_colourmap,edgecolors='none', alpha = alpha)
-                    sc = self.ax_list[0].scatter(easting,northing,s=point_scale, c=this_data,cmap=this_colourmap,norm=cNorm,edgecolors='none', alpha = alpha,zorder=zorder)
+                    sc = self.ax_list[0].scatter(easting,northing,s=point_scale, c=this_data,cmap=this_colourmap,norm=cNorm,edgecolors='none', alpha = alpha,zorder=zorder, marker = marker)
 
                 else:
                     print("Your colour_log_manual_scale should be something like [min,max], aborting")
                     quit()
             else:
-                sc = self.ax_list[0].scatter(easting,northing,s=point_scale, c=this_data,cmap=this_colourmap,edgecolors='none', alpha = alpha,zorder=zorder)
+                sc = self.ax_list[0].scatter(easting,northing,s=point_scale, c=this_data,cmap=this_colourmap,edgecolors='none', alpha = alpha,zorder=zorder, marker = marker)
 
         # Setting the labelling
         if(label_field != "None"):
