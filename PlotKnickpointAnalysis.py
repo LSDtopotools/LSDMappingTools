efb01bc61218e5541b163e1d08c566421394045c
Boris Gailleton <b.gailleton@sms.ed.ac.uk>
2018-03-01 18:38:08 +0000

    minor fixes

--- PlotKnickpointAnalysis.py
+++ PlotKnickpointAnalysis.py
@@ -157,6 +157,7 @@ def main(argv):
         args.river_profile = True
         args.raster_plots = True
         args.raster_plots_large_dataset = True
+        args.basin_plot = True
 
 
     # Plotting hte knickpoints
