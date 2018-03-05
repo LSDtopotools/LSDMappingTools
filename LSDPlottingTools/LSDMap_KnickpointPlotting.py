8f066ee6c261b888c601667945afff87e1ae6f94
bgailleton <boris.gailleton@gmail.com>
2018-03-05 19:03:21 +0000

    Added the stepped knickpoint to the map

--- LSDPlottingTools/LSDMap_KnickpointPlotting.py
+++ LSDPlottingTools/LSDMap_KnickpointPlotting.py
@@ -550,6 +550,7 @@ class KP_plotting(object):
 
         kp_pos = LSDP.LSDMap_PointData(self.df_kp[self.df_kp["sign"] == 1], data_type = "pandas", PANDEX = True)
         kp_neg = LSDP.LSDMap_PointData(self.df_kp[self.df_kp["sign"] == -1], data_type = "pandas", PANDEX = True)
+        kp_step = LSDP.LSDMap_PointData(self.df_kp_stepped[self.df_kp_stepped["delta_segelev"] > 0], data_type = "pandas", PANDEX = True)
 
         # Dealing with the legend that you can manually change
         if(len(extent_cmap) != 2):
@@ -570,6 +571,7 @@ class KP_plotting(object):
 
         MF.add_point_data(kp_pos,this_colourmap = "autumn",colour_manual_scale = [extent_cmap[0],extent_cmap[1]], marker ="^", column_for_plotting = "delta_ksn", color_abs = True ,show_colourbar=True, colorbarlabel = r'$\Delta k_{sn}$', colourbar_location = "bottom", scale_points = scale_points, scaled_data_in_log= False, column_for_scaling = 'delta_ksn',scale_in_absolute = True ,alpha=1,max_point_size = 15,min_point_size = 1,zorder=200,manual_size = size_kp)
         MF.add_point_data(kp_neg,this_colourmap = "autumn",colour_manual_scale = [extent_cmap[0],extent_cmap[1]], marker ="v", column_for_plotting = "delta_ksn", color_abs = True ,show_colourbar="False", scale_points = scale_points, scaled_data_in_log= False, column_for_scaling = 'delta_ksn',scale_in_absolute = True ,alpha=1,max_point_size = 15,min_point_size = 1,zorder=200,manual_size = size_kp)
+        MF.add_point_data(kp_neg,unicolor = "#00FF42",marker ="|", column_for_plotting = "none", alpha=1,max_point_size = 15,min_point_size = 1,zorder=200,manual_size = size_kp*0.5)
 
         if(black_bg):
             suffix = "dark"
